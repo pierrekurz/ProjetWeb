9d1f1ecdc7b489f46badfdfe424bf4e3a91d37dc
David <davkletz@gmail.com>
2019-03-28 22:55:18 +0100

    Ajout Endpoints
    Ajout des endpoints pour recherches sur valeurs et sur plusieurs colonnes

--- dant/src/main/java/com/dant/app/Endpoint.java
+++ dant/src/main/java/com/dant/app/Endpoint.java
@@ -1,11 +1,21 @@
 package com.dant.app;
+
+import com.dant.entity.Account;
 import com.dant.entity.Colonne;
+import com.dant.entity.CsvParser;
+import com.dant.entity.Ligne;
 import com.dant.entity.Table;
+import com.dant.entity.lecteurTable;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * Created by pitton on 2017-02-20.
@@ -14,34 +24,68 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class Endpoint {
-	Table t;
+	final lecteurTable l = new lecteurTable();
+
+	static Map<String, Table> tables = new HashMap<String, Table>();
+
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String helloWorld() {
 		return "Hello World";
 	}
-	
+
 	@PUT
 	@Path("/newtable")
-	@Produces(MediaType.TEXT_HTML)
+	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void tableCreated() {
-		t=Table.getInstance();
-		System.out.println("test creation table");
-	}
-	
-	@PUT
-	@Path("/newtable/newcolonne")
-	@Produces(MediaType.TEXT_HTML)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public void colonneCreated(@QueryParam("name") String name,@QueryParam("type") String type ) {
-		Colonne c = new Colonne(name,type);
-		if(t != null) {
-			t.addColonne(c);
-			System.out.println("Colonne ajoutée name : " + c.name +" type : " + c.type);
+	public void tableCreated(@QueryParam("name") String name, Ligne ligne) {
+		Table t = new Table(name);
+		if (!tables.containsKey(name)) {
+			tables.put(name, t);
+			System.out.println("Table " + name + " crée");
+			System.out.println("ligne " + ligne + " crée");
+
 		}
+		System.out.println(tables.toString());
 	}
-	
+
+	@PUT
+	@Path("/{tablename}/newColonne")
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void colonneCreated(@PathParam("tablename") String tablename, @QueryParam("colonnename") String colonnename,
+			@QueryParam("colonnetype") String colonnetype) {
+		Colonne c = new Colonne(colonnename, colonnetype);
+		Table t = tables.get(tablename);
+		try {
+			t.addColonne(colonnename, colonnetype);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(
+				"Colonne ajoutée name : " + c.getName() + " type : " + c.getType() + " dans la table : " + tablename);
+
+	}
+
+
+
+
+	/*
+	 * @PUT
+	 * 
+	 * @Path("/{tablename}/insert")
+	 * 
+	 * @Produces(MediaType.TEXT_HTML)
+	 * 
+	 * @Consumes(MediaType.APPLICATION_JSON) public void
+	 * insert(@PathParam("tablename") String tablename) {
+	 * System.out.println("Received account " + account); Table
+	 * t=tables.get(tablename);
+	 * 
+	 * t.insert(account);
+	 * 
+	 * }
+	 */
 	@GET
 	@Path("/list")
 	public List<String> getListInParams(@QueryParam("ids") List<String> ids) {
@@ -49,11 +93,50 @@ public class Endpoint {
 		return ids;
 	}
 
+	@GET
+	@Path("/readTable")
+	public void readTable(@QueryParam("nameTable") String nameTable, @QueryParam("nameColumn") String nameColumn,
+			@QueryParam("element") String element) {
+		Table t = tables.get(nameTable);
+		l.lecteurTable(t, nameColumn, element);
+	}
+
+
+	@GET
+	@Path("/getElementsFigures")
+	public void readTable(@QueryParam("nameTable") String nameTable, @QueryParam("nameColumn") String nameColumn,
+			@QueryParam("condition") String condition, @QueryParam("value") String value) {
+		Table t = tables.get(nameTable);
+		l.getLinesByConditionsNumber(t, nameColumn, condition, value);
+	}
+
+
+	@GET
+	@Path("/getElementsByColumns")
+	public void readTable(@QueryParam("nameTable") String nameTable, @QueryParam("nameColumn") List<String> nameColumns,
+			@QueryParam("element") String element) {
+		Table t = tables.get(nameTable);
+		l.getLinesByConditionsNumber(t, nameColumns, nameColumns);
+	}
 
 	@GET
 	@Path("/exception")
 	public Response exception() {
 		throw new RuntimeException("Mon erreur");
+	}
+
+	@POST
+	@Path("/parsecsv")
+	public void parseCsv() throws Exception {
+		CsvParser.parseCsv();
+	}
+
+	@POST
+	@Path("/entity")
+	public Account getAccount(Account account) {
+		System.out.println("Received account " + account);
+		account.setUpdated(System.currentTimeMillis());
+		return account;
 	}
 
 }
--- dant/src/main/java/com/dant/entity/lecteurTable.java
+++ dant/src/main/java/com/dant/entity/lecteurTable.java
@@ -3,20 +3,97 @@ import com.google.gson.JsonObject;
 
 
 public class lecteurTable {
+	
+	Table currentTable;
+	String nameColumn;
+	String element;
+	Table returnTable;
+	
+	
 
 	
-	public Table lecteurTable(Table nameTable, String nameColumn, String element) {
+	public void lecteurTable(Table nameTable, String nameColumn, String element) {
 
-		this.currentTable = nameTable
-		this.nameColumn = nameColumn
-		this.element = element
-		this.returnTable = new Table("table indexes" + nameColumn + " element recherche : " + element)
+		this.nameColumn = nameColumn;
+		this.element = element;
+		returnTable = new Table("table indexes" + nameColumn + " element recherche : " + element);
 
 		for (int j = 0; j < this.currentTable.cols.size(); j++){
+			if (nameTable.elementsTable.get(j).get(nameColumn).contains(element )){
+				returnTable.insert(nameTable.elementsTable.get(j));
+				System.out.println(nameTable.elementsTable.get(j));
+			}
 
-			if (this.element in this.currentTable.elements[j].get(this.nameColumn)){
+		}
 
-				this.returnTable.add(this.currentTable[j])
+	}
+
+
+	public void getLinesByConditionsNumber(Table nameTable, String nameColumn, String condition, Int value){
+		// function to get elements from a condition on its value 
+
+		returnTable = new Table("table indexes" + nameColumn + " element recherche : " + element);
+		Colonne currentColonne = nameTable.cols.get(nameColumn);
+
+		if currentColonne.type != "int"{
+			// the column doesn t contains figure so it is impossible 
+			System.out.println('error : not int in selected column');
+		}
+
+		else{
+
+			for (k=0; k< nameTable.elementsTable; k++){
+				// for each element in the column, we are going to test if the condition is validated
+
+				if(condition == "=" && nameTable.elementsTable.get(k).get(nameColumn) == value){
+					// condiiton of equality
+					returnTable.insert(nameTable.elementsTable.get(k));
+					System.out.println(nameTable.elementsTable.get(k));
+				}
+				else if(condition == ">" && nameTable.elementsTable.get(k).get(nameColumn) > value){
+					// condition to be bigger than a value
+					returnTable.insert(nameTable.elementsTable.get(k));
+					System.out.println(nameTable.elementsTable.get(k));
+				}
+
+				else if(condition == "<" && nameTable.elementsTable.get(k).get(nameColumn) < value){
+					// condition to be smaller than a value
+					returnTable.insert(nameTable.elementsTable.get(k));
+					System.out.println(nameTable.elementsTable.get(k));
+				}
+
+				else if(condition == ">=" && nameTable.elementsTable.get(k).get(nameColumn) >= value){
+					// condition to be bigger or equal to a value
+					returnTable.insert(nameTable.elementsTable.get(k));
+					System.out.println(nameTable.elementsTable.get(k));
+				}
+
+				else if(condition == "<=" && nameTable.elementsTable.get(k).get(nameColumn) <= value){
+					// condition to be smaller or equal to a value
+					returnTable.insert(nameTable.elementsTable.get(k));
+					System.out.println(nameTable.elementsTable.get(k));
+				}
+
+			}
+
+		}
+
+	}
+
+
+
+	public void searchOnManyColumns(Table nameTable, List<String> nameColumns, String element) {
+		// function to get all the lines of the table with the element in any column of the given list
+
+		returnTable = new Table("table indexes" + nameColumn + " element recherche : " + element);
+
+		for (int j = 0; j < this.currentTable.cols.size(); j++){
+			//for each line of the column
+			for (int k=0; k < nameColumns.size();k++){
+				if (nameTable.elementsTable.get(j).get(nameColumns.get(k)).contains(element )){
+					returnTable.insert(nameTable.elementsTable.get(j));
+					System.out.println(nameTable.elementsTable.get(j));
+				}
 			}
 
 		}
