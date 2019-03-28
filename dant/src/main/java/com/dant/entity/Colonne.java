a698f6ad09d8478af3456aa76bd8ce16c1b99190
David <davkletz@gmail.com>
2019-03-28 22:57:46 +0100

    reorganisation fichiers

--- dant/src/main/java/com/dant/entity/Colonne.java
+++ dant/src/main/java/com/dant/entity/Colonne.java
@@ -1,13 +1,21 @@
 package com.dant.entity;
 
 public class Colonne {
-	public final String name;
-	public final String type;
+	private String name;
+
+
+	private String type;
 	
+
+
 	public Colonne(String name, String type){
 		this.name= name;
 		this.type= type;
 	}
-	
-	
+		public String getType() {
+		return type;
+	}
+		public String getName() {
+		return name;
+	}
 }
