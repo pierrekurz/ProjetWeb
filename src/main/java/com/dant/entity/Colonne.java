0568b2570d0a66807c6dc9968b6fd68ca48359a0
pierrekurz <pkouze@gmail.com>
2019-03-29 14:55:30 +0100

    Revert "nettoyage projet"
    This reverts commit 953ba799ac1316257e2e1529e1255855e907d62f.

---
+++ src/main/java/com/dant/entity/Colonne.java
@@ -0,0 +1,13 @@
+package com.dant.entity;
+
+public class Colonne {
+	public final String name;
+	public final String type;
+	
+	public Colonne(String name, String type){
+		this.name= name;
+		this.type= type;
+	}
+	
+	
+}
---
+++ src/main/java/com/dant/entity/Index.java
@@ -0,0 +1,12 @@
+package com.dant.entity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Index {
+	Map<String, Colonne> colonne = new HashMap<>();
+	
+	public Index() {
+		
+	}
+}
