318498866393bf27bb72df11b5741249c5c8b474
David <davkletz@gmail.com>
2019-06-02 22:55:53 +0200

    MAJ endpoint
    Reimport de l'endpoint

--- src/main/java/com/dant/app/Endpoint.java
+++ src/main/java/com/dant/app/Endpoint.java
@@ -1,7 +1,9 @@
 package com.dant.app;
 
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -14,10 +16,13 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.jboss.logging.Logger;
+
 import com.dant.entity.Table;
 //import org.json.simple.JSONObject;
 import com.google.gson.*;
 
+import Controllers.CsvParser;
 import Controllers.Repartisseur;
 
 /**
@@ -29,53 +34,161 @@ import Controllers.Repartisseur;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class Endpoint {
+	CsvParser csvReader=null;
 	Repartisseur repartisseur = new Repartisseur(true);
-	
 	@POST
 	@Path("/createMainNode")
 	public void initNodes() throws Exception {
 		System.out.println("test create mainNode");
-
 		repartisseur = new Repartisseur(true);
 	}
 	
 	@POST
 	@Path("/joinMainNode")
-	public void joinNodes() throws Exception {
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void joinNodes(@QueryParam("mainPort") int mainPort, @QueryParam("portNb") int portSecond) throws Exception {
 		System.out.println("test join mainNode");
 		repartisseur = new Repartisseur(false);
+		repartisseur.joinMainNode(mainPort, portSecond);
 	}
+	
+	
+	@GET
+	@Path("/connect")
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void connect(
+			@QueryParam("portNb") int portSecond
+			) throws Exception {
+		
+		repartisseur.addNode(portSecond);
+	}
+	
+	
+	@POST
+	@Path("/parse")
+	//@Produces(MediaType.TEXT_HTML)
+	@Consumes("text/csv")
+	public Response parseCsv(
+			@QueryParam("pathToFile") String pathToFile, 
+			@QueryParam("nameOfFile") String nameOfFile
+			) throws Exception {
+		//csvReader = new CsvParser(pathToFile, nameOfFile);
+		repartisseur.ParceCSV(pathToFile, nameOfFile);
+		//repartisseur.getListTables().add(csvReader.getTable());
+
+		System.out.println("Fichier Parse");
+		return Response.status(200).entity("ok").build();
+	}
+	
+	
+	@GET
+	@Path("/headers")
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void headers(
+			@QueryParam("listHeaders") String listHeaders, 
+			@QueryParam("nameTable") String nameTable
+			) throws Exception {
+		repartisseur.addHeader(listHeaders, nameTable);
+	}
+	
+	
 	
 	@GET
 	@Path("/test")
 	@Produces(MediaType.TEXT_HTML)
 	public String helloWorld() {
+		
 		return "<b>Hello World</b>";
 	}
 	
-	@PUT
-	@Path("/parse/{path}")
-	@Produces(MediaType.TEXT_HTML)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public void parseCsv(@QueryParam("path") String path) throws Exception {
-		repartisseur.ParceCSV(path);
-		System.out.println("Fichier Parse");
+	
+	@POST
+	@Path("/addLines")
+	//@Produces(MediaType.TEXT_HTML)
+	@Consumes("text/csv")
+	public Response addLines(
+			@QueryParam("lines") String lines, 
+			@QueryParam("nameFile") String nameFile
+			) throws Exception {
+		repartisseur.addLines(lines, nameFile);
+		return Response.status(200).entity("ok").build();
 	}
 	
 	
+	
 	@PUT
-	@Path("/addIndex/{nameIndex}")
+	@Path("/addIndex")
 	@Produces(MediaType.TEXT_HTML)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void createIndex(@QueryParam("nameIndex")  String nameIndex) throws Exception {
+	public Response createIndex(
+			@QueryParam("nameIndex")  String nameIndex, 
+			@QueryParam("nameFile")  String nameFile
+			
+			) throws Exception {
 		
 		List<String> listNameColumns = new ArrayList<String>();
 		//for (String word : nameIndex ) {
-			//listNameColumns.add(word);
+			listNameColumns.add(nameIndex);
 		//}
-		repartisseur.addIndex(listNameColumns);
+		repartisseur.addIndex(listNameColumns, nameFile);
+		//repartisseur.sendInstructions("");
 		System.out.println("Index ajoute");
+		System.out.println(repartisseur.getListTables().get(0).toString());
+		return Response.status(200).entity("ok").build();
+ 
 	}
+	
+	
+	
+	
+	
+	
+	@GET
+	@Path("/get")
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String get(
+			@QueryParam("nameIndex") String nameIndex,
+			@QueryParam("value")  String value
+			) throws Exception {
+		List<String> listIndex=new ArrayList<String>();
+		List<String> listValue = new ArrayList<String>();
+		
+		int sizeOfIndex = nameIndex.length();
+		String word = "";
+		for(int l = 0; l<sizeOfIndex; l++) {
+			if(nameIndex.charAt(l) == '/') {
+				listIndex.add(word);
+				l++;
+			}
+			else {
+				word+=nameIndex.charAt(l);
+			}
+		}
+		
+		
+		int sizeOfValue = value.length();
+		String wordValue = "";
+		for(int l = 0; l<sizeOfIndex; l++) {
+			if(value.charAt(l) == '/') {
+				listValue.add(wordValue);
+				l++;
+			}
+			else {
+				wordValue+=value.charAt(l);
+			}
+		}
+				
+		String results = repartisseur.get(listIndex, listValue);
+		return results;
+	}
+	
+	
+	
+	
 	
 	@PUT
 	@Path("/searchSmaller")
@@ -85,7 +198,8 @@ public class Endpoint {
 			@QueryParam("nameIndex") String nameIndex,
 			@QueryParam("valueMax")  int valueMax
 			) throws Exception {
-		List<Object[]> listNbLines = repartisseur.searchSmaller(nameIndex, valueMax);
+		List<Object[]> listNbLines = new ArrayList<Object[]>();
+		//List<Object[]> listNbLines = repartisseur.searchSmaller(nameIndex, valueMax);
 		return listNbLines;
 		//return (listNbLines.);
 	}
@@ -94,23 +208,28 @@ public class Endpoint {
 	@Path("/searchBigger")
 	@Produces(MediaType.TEXT_HTML)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public JsonObject searchBigger(
+	public List<Object[]> searchBigger(
 			@QueryParam("nameIndex") String nameIndex,
 			@QueryParam("valueMin")  int valueMin
 			) throws Exception {
-		List<Object[]> listNbLines = repartisseur.searchSmaller(nameIndex, valueMin);
-		return null;
+		List<Object[]> listNbLines = new ArrayList<Object[]>();
+		//List<Object[]> listNbLines = repartisseur.searchSmaller(nameIndex, valueMin);
+		return listNbLines;
 		//return (listNbLines.toJson());
 	}
 	
 	@GET
 	@Path("/tables")
-	public List<Table> getTables() {
+	public HashMap<String, Controllers.Table> getTables() {
 		return repartisseur.getListTables();
 		
 	}
 	
-	
+	@GET
+	@Path("/download")
+	public Response getDl() {
+		return Response.status(200).entity("ok").build();
+	}
 	
 	
 		
